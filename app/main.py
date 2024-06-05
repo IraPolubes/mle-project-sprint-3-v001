686e295a6eaa30528c64e2e1711a4823b6d6876d
Irina Poloubessov <polubes@gmail.com>
2024-06-05 07:30:35 +0000

    grafana in progress

--- app/main.py
+++ app/main.py
@@ -1,11 +1,57 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, HTTPException
 from .flat_price_handler import FlatPriceHandler
+import logging
+from prometheus_fastapi_instrumentator import Instrumentator
+from prometheus_client import Counter, Histogram, Gauge
+import psutil
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 app =FastAPI()
 
 app.handler = FlatPriceHandler()
 
+Instrumentator().instrument(app).expose(app)
+
+# счетчик положительных предсказаний -значение больше 0 значит предсказание прошло успешно
+total_requests = Counter('total_requests', 'Total number of requests')
+
+# счетчик предсказаний цены 
+successful_requests = Counter('successful_requests', 'Number of successful requests') 
+
+# общие значения предсказаний
+flat_app_predictions = Histogram('main_app_predictions', 'Prediction values')  
+
+# измеритель ресурса памяти
+memory_usage_gauge = Gauge('memory_usage', 'Memory usage of the application')
+
+# измеритель ресурса cpu
+cpu_usage_gauge = Gauge('cpu_usage', 'CPU usage of the application')
+
+"""
+Далее в Графане на основе этих метрик будет посчитана доля удачно обработанных запросов
+через использование PromQL
+rate(successful_requests[1m]) / rate(total_requests[1m])
+
+"""
+
 @app.post('/api/predict/')
 def get_prediction(model_params: dict):
-    return app.handler.handle(model_params)
+    try:
+        prediction = app.handler.handle(model_params)
+    except Exception as e:
+        logging.error(f"Problem with request: {e}")
+        raise HTTPException(status_code=500, detail=f'Problem with request: {e}')
+
+    if 'predicted price' not in prediction:
+        logging.error("Prediction does not contain 'predicted price'")
+        raise HTTPException(status_code=500, detail='Prediction does not contain predicted price')
+    total_requests.inc()
+    if prediction['predicted price'] > 0:
+            logging.info(f"Successful prediction: {prediction['predicted price']}")
+            successful_requests.inc()
+    
+    memory_usage_gauge.set(psutil.virtual_memory().percent)  # Использование памяти
+    cpu_usage_gauge.set(psutil.cpu_percent())  # Использование CPU
+
+    return prediction
--- generate_requests.py
+++ generate_requests.py
@@ -1,10 +1,42 @@
 import requests
+import random
 import time
 
-for i in range(3):
-    params = {
-        'x': str(i),
-        'y': '2',
+url = 'http://localhost:8081/api/predict/'
+
+def generate_random_data():
+    """ Генерация случайных данных в формате ввода для предсказания. """
+    return {
+        "id": random.randint(1, 1000),
+        "flat_id": random.uniform(0, 1),
+        "building_id": random.uniform(1000, 7000),
+        "floor": random.randint(1, 30),
+        "is_apartment": random.choice([0, 1]),
+        "kitchen_area": random.uniform(5, 20),
+        "living_area": random.uniform(10, 50),
+        "rooms": random.randint(1, 5),
+        "studio": random.choice([0, 1]),
+        "total_area": random.uniform(20, 100),
+        "build_year": random.randint(1900, 2022),
+        "building_type_int": random.randint(1, 10),
+        "latitude": random.uniform(55.0, 56.0),
+        "longitude": random.uniform(37.0, 38.0),
+        "ceiling_height": random.uniform(2.0, 3.5),
+        "flats_count": random.randint(1, 100),
+        "floors_total": random.randint(1, 30),
+        "has_elevator": random.choice([0, 1])
     }
-    response = requests.get('http://localhost:1702/predict', params=params)
-    time.sleep(10)
+
+
+def simulate_load(number_of_requests, delay_between_requests=0):
+    """Отправка запросов в указанном количестве. """
+    for _ in range(number_of_requests):
+        data = generate_random_data()
+        response = requests.post(url, json=data)
+        print(f'Status Code: {response.status_code}, Prediction: {response.json()}')
+        if delay_between_requests > 0:
+            time.sleep(delay_between_requests)
+
+
+if __name__ == '__main__':
+    simulate_load(100,1)
